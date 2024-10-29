f9423069dc8cf8fb3b354501fe80d59fc5d24ccb
cflab2017 <cflab@naver.com>
2024-10-29 12:08:19 +0900

    aa

--- game/spacewar/drawBg.py
+++ game/spacewar/drawBg.py
@@ -3,18 +3,17 @@ from pygame.locals import *
 from pygame import Surface
 
 class DrawBg():
-	def __init__(self,screen:Surface,filename, rect:pygame.Rect):
+	def __init__(self,screen:Surface):
 		self.screen = screen
-		self.filename = filename
-		img = pygame.image.load(f'{filename}')
-		self.image = pygame.transform.scale(img, (rect.width, rect.height))
-		self.rect = self.image.get_rect()
-		self.rect.x = rect.x
-		self.rect.y = rect.y
+		# self.filename = filename
+		# img = pygame.image.load(f'{filename}')
+		# self.image = pygame.transform.scale(img, (rect.width, rect.height))
+		# self.rect = self.image.get_rect()
+		self.rect = pygame.Rect(0,0,self.screen.get_width(), self.screen.get_height())
 		self.rect2 = self.rect.copy()
-		self.width = rect.width
+		self.width = self.rect.width
 
-	def draw(self):
+	def draw(self, image):
 		# pygame.draw.rect(self.screen,(255,255,255),rect,1)  
 		
 		self.rect.x += 1
@@ -22,5 +21,5 @@ class DrawBg():
 			self.rect.x = 0
 		self.rect2.right = self.rect.left
 		
-		self.screen.blit(self.image,self.rect2)
-		self.screen.blit(self.image,self.rect)
+		self.screen.blit(image,self.rect2)
+		self.screen.blit(image,self.rect)
